fe0c9d530673911cd1e4dfc06a22a72e3410064c
Mariona Farré Tapias <marionafarretapias@gmail.com>
2023-12-11 11:37:34 +0100

    documents+altres fitxers

---
+++ lab5/CountWords.py
@@ -0,0 +1,61 @@
+"""
+.. module:: CountWords
+
+CountWords
+**********
+
+:Description: CountWords
+
+    Generates a list with the counts and the words in the 'text' field of the documents in an index
+
+:Version: 
+
+:Date:  04/09/2023
+
+"""
+
+from __future__ import print_function
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import scan
+from elasticsearch.exceptions import NotFoundError, TransportError
+
+import argparse
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--index', default=None, required=True, help='Index to search')
+    parser.add_argument('--alpha', action='store_true', default=False, help='Sort words alphabetically')
+    args = parser.parse_args()
+
+    index = args.index
+
+    try:
+        client = Elasticsearch()
+        voc = {}
+        sc = scan(client, index=index, query={"query" : {"match_all": {}}})
+        for s in sc:
+            try:
+                tv = client.termvectors(index=index, id=s['_id'], fields=['text'])
+                if 'text' in tv['term_vectors']:
+                    for t in tv['term_vectors']['text']['terms']:
+                        if t in voc:
+                            voc[t] += tv['term_vectors']['text']['terms'][t]['term_freq']
+                        else:
+                            voc[t] = tv['term_vectors']['text']['terms'][t]['term_freq']
+            except TransportError:
+                pass
+        lpal = []
+
+        total = 0
+        for v in voc:
+            lpal.append((v.encode("utf-8", "ignore"), voc[v]))
+            total += voc[v]
+
+        
+        for pal, cnt in sorted(lpal, key=lambda x: x[0 if args.alpha else 1]):
+            print(f'{cnt}, {pal.decode("utf-8")}')
+        print('-----------------------------------------------')
+        print(f'{len(lpal)} Distinct words, {total} Word occurrences')
+    except NotFoundError:
+        print(f'Index {index} does not exist')
